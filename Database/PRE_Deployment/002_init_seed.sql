fab3ff1ac576aeff0ae8149f45bcd76b2f20175d
McSlimer <kumarsharma04122004@gmail.com>
2025-10-08 01:43:08 +0200

    feat(Database): Update initial seed

--- Database/PRE_Deployment/002_init_seed.sql
+++ Database/PRE_Deployment/002_init_seed.sql
@@ -1,18 +1,19 @@
+-- Select database
 USE ToDoDB;
 
--- Roles
+-- Insert initial roles
 INSERT IGNORE INTO Role (Id, Name) VALUES
   (1, 'Admin'),
   (2, 'User'),
   (3, 'Manager');
 
--- Priorities
+-- Insert initial priorities
 INSERT IGNORE INTO Priority (Id, Name) VALUES
   (1, 'High'),
   (2, 'Medium'),
   (3, 'Low');
 
--- Statuses
+-- Insert initial statuses
 INSERT IGNORE INTO Status (Id, Name) VALUES
   (1, 'Pending'),
   (2, 'In Progress'),
